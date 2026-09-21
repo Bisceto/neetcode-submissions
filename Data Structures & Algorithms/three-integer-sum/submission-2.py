2268a215e84eb9f80f2a8dd46335580203f75074
Wesley Teo <88726470+Bisceto@users.noreply.github.com>
2026-09-21 14:56:06 +0800

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        print(nums)
+        res = []
+        for idx, val in enumerate(nums):
+            # Duplicated third number, will return duplicate triplet
+            if idx != 0 and val == nums[idx - 1]:
+                continue
+            if idx == len(nums) - 2:
+                break
+            left = idx + 1
+            left_val = None
+            right = len(nums) - 1
+            right_val = None
+            while left < right:
+                if nums[left] == left_val and nums[right] == right_val:
+                    left += 1
+                    right -= 1
+                    continue
+                two_sum = nums[left] + nums[right]
+                if 0 - val == two_sum:
+                    res.append([val, nums[left], nums[right]])
+                    left_val = nums[left]
+                    right_val = nums[right]
+                    left += 1
+                    right -= 1
+                # Tuple needs to be bigger
+                elif 0 - val > two_sum:
+                    left += 1
+                else:
+                    right -= 1
+        return res
+
+
+
+            
