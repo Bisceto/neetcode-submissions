d99d574e07add71585c1507a97508c71017078df
Wesley Teo <88726470+Bisceto@users.noreply.github.com>
2026-09-23 20:38:23 +0800

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # key observation, a car behind can never arrive before a car in front. they will only arrive together
+        # stack is overkill
+        # st = []
+        # cars = sorted(zip(position, speed), key=lambda pair: pair[0], reverse=True)
+        # # Going from the closest car to the furthest car. So nobody can overtake this first car.
+        # for pos, spd in cars:
+        #     time = (target - pos) / spd
+        #     # Condition to check if behind car can catch up to front car before target
+        #     if st and time <= st[-1][2]:
+        #         continue
+        #         # These cars will all join the fleet and just move at the fleet's pace. So we dont add anything
+        #     else:
+        #         st.append((pos, spd, time))
+        # return len(st)
+        fleets, prev_time = 0, 0
+        cars = sorted(zip(position, speed), key=lambda pair: pair[0], reverse=True)
+        # Going from the closest car to the furthest car. So nobody can overtake this first car.
+        for pos, spd in cars:
+            time = (target - pos) / spd
+            # Condition to check if behind car can catch up to front car before target
+            if time <= prev_time:
+                continue
+                # These cars will all join the fleet and just move at the fleet's pace. So we dont add anything
+            else:
+                fleets += 1
+                prev_time = time
+        return fleets
